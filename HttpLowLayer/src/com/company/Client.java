93cf7e70bd25834a12f9799f472d0d5b59e44a69
AsadaGuitar <79627592+AsadaGuitar@users.noreply.github.com>
2021-06-29 01:11:32 +0900

    変更点
    Logger の追加

--- HttpLowLayer/src/com/company/Client.java
+++ HttpLowLayer/src/com/company/Client.java
@@ -5,43 +5,28 @@ import java.io.IOException;
 import java.io.InputStreamReader;
 import java.io.PrintWriter;
 import java.net.Socket;
+import java.util.logging.Logger;
 
 public class Client {
+
     public static void main(String[] args) throws IOException {
 
-        Socket socket = new Socket("localhost",80);
-        System.out.println("START SOCKET");
+        Logger log = Logger.getLogger("Client.main");
+        log.info("Application Starts");
+
+        Socket socket = new Socket("localhost", 80);
+        log.info("CONNECTION STARTS");
         PrintWriter writer = new PrintWriter(socket.getOutputStream(), true);
 
         writer.println("Hello World");
+        log.info("Send \"Hello World\"");
 
         BufferedReader reader = new BufferedReader(
                 new InputStreamReader(socket.getInputStream()));
         String line = reader.readLine();
-        System.out.println("line : " + line);
+        log.info("Received Data Is ..." + line);
 
-//        try (
-//                PrintWriter writer = new PrintWriter(socket.getOutputStream(), true);
-//                BufferedReader reader = new BufferedReader(
-//                        new InputStreamReader(socket.getInputStream()))
-////                BufferedReader keyboard = new BufferedReader(
-////                        new InputStreamReader(System.in)))
-//            {
-
-//                while (true) {
-//                    System.out.print("Input＞");
-//                    String input = keyboard.readLine();
-//                    writer.println(input);
-//                    if (input.equals("¥¥q")) {
-//                        break;
-//                    }
-//                    System.out.println("［Server］" + reader.readLine());
-//                }
-            }
-//        catch (Exception e){
-//            e.printStackTrace();
-//        }
-
-
-//    }
+        socket.close();
+        log.info("Socket Is Closed");
+    }
 }
